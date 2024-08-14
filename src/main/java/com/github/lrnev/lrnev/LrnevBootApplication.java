4d7c8786815954415231f142f33d999a7be31945
LuChangQiu <17349798806@163.com>
2024-08-14 21:07:58 +0800

    init

---
+++ src/main/java/com/github/lrnev/lrnev/LrnevBootApplication.java
@@ -0,0 +1,13 @@
+package com.github.lrnev.lrnev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LrnevBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LrnevBootApplication.class, args);
+    }
+
+}
